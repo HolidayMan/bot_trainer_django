29b51d4bad806e1a4d1fc5fd9f0775813072edf6
HolidayMan <lehafirstchanel@gmail.com>
2020-02-10 10:11:10 +0200

    changed saving mechanism

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,23 +1,43 @@
 import pickle
+import os
 
-
-class UserTaskSaver:
-    filename = "./user_tasks.save"
-    _user_task = {}
+class Saver:
+    folder = "./saves/"
+    filename = None
+    _data = None
 
     def __init__(self):
+        self.filename = self.folder + self.filename
         self.upload()
 
+
     def save(self):
+        if not os.path.exists(self.folder):
+            os.mkdir(self.folder)
+
         with open(self.filename, "wb") as file:
             pickle.dump(self._user_task, file)
 
     def upload(self):
+        if not os.path.exists(self.folder):
+            os.mkdir(self.folder)
         try:
             with open(self.filename, "rb") as file:
-                self._user_task = pickle.load(self.filename)
+                self._data = pickle.load(self.filename)
         except FileNotFoundError:
-            self._user_task = {}
+            self._data = {}
+
+    def __del__(self):
+        self.save()
+
+
+class UserTaskSaver(Saver):
+    filename = "user_tasks.save"
+    _user_task = {}
+    data = _user_task
+
+    def __init__(self):
+        super().__init__()
 
     def __iter__(self):
         return iter(self._user_task.values())
@@ -33,6 +53,3 @@ class UserTaskSaver:
 
     def setdefault(self, item, default):
         return self._user_task.setdefault(item, default)
-
-    def __del__(self):
-        self.save()
